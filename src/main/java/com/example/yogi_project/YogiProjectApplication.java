9922e5010d35f8deca05901ca0b06ba9e831fd0c
hyowon3434 <hyowon3434@naver.com>
2024-09-23 22:12:19 +0900

    first commit

---
+++ src/main/java/com/example/yogi_project/YogiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.yogi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogiProjectApplication.class, args);
+    }
+
+}
