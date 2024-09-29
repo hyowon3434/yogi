0fa5e36b8e76fac5052c9db5af3c4124f45fe697
Hyowon Kang <hyowon3434@naver.com>
2024-09-29 11:09:33 +0900

    Fix: ToString 추가

--- src/main/java/com/example/yogi_project/reservation/domain/ReservationVO.java
+++ src/main/java/com/example/yogi_project/reservation/domain/ReservationVO.java
@@ -3,12 +3,14 @@ package com.example.yogi_project.reservation.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.apache.ibatis.type.Alias;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Alias("ReservationVO")
+@ToString
 public class ReservationVO {
     private Integer reservationId;
     private String email;
--- src/main/java/com/example/yogi_project/review/domain/ReviewVO.java
+++ src/main/java/com/example/yogi_project/review/domain/ReviewVO.java
@@ -3,12 +3,14 @@ package com.example.yogi_project.review.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.apache.ibatis.type.Alias;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Alias("ReviewVO")
+@ToString
 public class ReviewVO {
     private Integer reviewId;
     private Integer accommodationId;
